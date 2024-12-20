1fbee52da20ee40b5105a1fd9275d60c3fec3a0b
Samuel de Vega <svgarcia@idealista.com>
2024-12-20 12:15:00 +0000

    typescript setup

---
+++ typescript/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testMatch: ['**/test/**/*.test.ts']
+}
